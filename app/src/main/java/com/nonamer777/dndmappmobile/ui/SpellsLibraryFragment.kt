e2588ba7e8acb65d70075495028537f1e50107b9
Oscar Wellner <o.wellner@telfort.nl>
2020-12-16 14:25:01 +0100

    show spells fetched from firebase into the list

--- app/src/main/java/com/nonamer777/dndmappmobile/ui/SpellsLibraryFragment.kt
+++ app/src/main/java/com/nonamer777/dndmappmobile/ui/SpellsLibraryFragment.kt
@@ -5,10 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nonamer777.dndmappmobile.R
 import com.nonamer777.dndmappmobile.databinding.FragmentSpellsLibraryBinding
+import com.nonamer777.dndmappmobile.model.Spell
+import com.nonamer777.dndmappmobile.ui.adapter.SpellAdapter
 import com.nonamer777.dndmappmobile.ui.viewModel.SpellViewModel
 
 /**
@@ -19,6 +23,10 @@ class SpellsLibraryFragment: Fragment() {
     private lateinit var binding: FragmentSpellsLibraryBinding
 
     private val spellViewModel: SpellViewModel by activityViewModels()
+
+    private val spells = arrayListOf<Spell>()
+
+    private val spellAdapter = SpellAdapter(spells, ::onSpellClick)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +43,13 @@ class SpellsLibraryFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.spellList.adapter = spellAdapter
+        binding.spellList.layoutManager = LinearLayoutManager(
+            context,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
         observeRequests()
 
         spellViewModel.getSpells(requireActivity())
@@ -42,7 +57,14 @@ class SpellsLibraryFragment: Fragment() {
 
     private fun observeRequests() {
         spellViewModel.spells.observe(viewLifecycleOwner, {
-            Log.d("SpellsLibraryFragment", it.toString())
+            spells.clear()
+            spells.addAll(it)
+
+            spellAdapter.notifyDataSetChanged()
         })
+    }
+
+    private fun onSpellClick(spell: Spell) {
+        Toast.makeText(context, "showing detail of ${spell.name}", Toast.LENGTH_SHORT).show()
     }
 }
